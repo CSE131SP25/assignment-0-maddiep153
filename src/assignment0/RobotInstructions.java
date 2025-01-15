3f913a18114baf1ab3e71cb858472662fd2eb691
madal <madal@chickensoup.dhcp.wustl.edu>
2025-01-15 15:28:56 -0600

    Assignment 0

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,14 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.forward(60);
+		robot.forward(60);
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(70);
 	}
 
 }
